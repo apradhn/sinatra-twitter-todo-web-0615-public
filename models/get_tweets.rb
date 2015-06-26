7a79bff0c4f5918bb00e661a0505c537c577a683
apradhn <abhijit.pradhan6@gmail.com>
2015-06-26 09:11:44 -0400

    all done

--- models/get_tweets.rb
+++ models/get_tweets.rb
@@ -11,10 +11,10 @@ class GetTweets
 
   TWITTER = Twitter::REST::Client.new do |config|
     # go to https://dev.twitter.com/apps and create your own twitter app. Then grab your keys and replace these ones
-    config.consumer_key = "SOo0mIfGvsFVP7OlrNVaRS7bE"
-    config.consumer_secret = "yzNnxhbZknAcIOWXiPAL7UWVrdqr3hBYgLJPs6sTqaYiCVEXHh"
-    config.access_token = "276237692-vwCB7oLa5TWBoDFv7MVdap6aDxzAqdVFbpyjrwQM"
-    config.access_token_secret = "iXQ7vSO7Rvq8zVnZO3u8LDRMQvTzcHCfTRDUTBB3ZNRk0"
+    config.consumer_key = "CzOLz1nKBUaZDL46Wn2Mi2KR8"
+    config.consumer_secret = "JEPGTamQCl2z9NgS6gAcb0giItYxZFBOfwMDUgnihKZd0dpuKZ"
+    config.access_token = "212275546-8Pg5NhZMigOYYyi5nQYlrGbybRQnByYemoTEFRng"
+    config.access_token_secret = "XihT5xyfKYfKfBuZXf6ejBneIpBfEK9rUXE8h4CDs5DMF"
   end
 
   def get_search_results
